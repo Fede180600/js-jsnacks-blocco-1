4a678f0054aaa1bb31bd3942ea64a794e5cda837
Fede180600 <fede180600@libero.it>
2022-04-19 15:28:43 +0200

    add empty array

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,7 +1,8 @@
 // jsnack2. Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
 
 // Creo la mia array vuota
-
+const myArray = [];
 // Chiedo 6 numeri all'utente 
 // Se è dispari lo inserisco nell'array
+
 
