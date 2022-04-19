8e254b01706356d0badd0107939e34508e3d6077
Fede180600 <fede180600@libero.it>
2022-04-19 15:54:29 +0200

    add numbers generator

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,7 +1,9 @@
 // jsnack3. Calcola la somma e la media dei primi 10 numeri.
 
 // Generare i primi 10 numeri 
-
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
 // Calcolarne la somma 
 
 
