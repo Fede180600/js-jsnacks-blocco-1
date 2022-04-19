f01e81d5cdde0f45156a1ffc7aaddc952872f27b
Fede180600 <fede180600@libero.it>
2022-04-19 15:03:10 +0200

    js pseudo-code jsnack1

---
+++ jsnack1/js/script.js
@@ -0,0 +1,6 @@
+// jsnack1. L'utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// Chiedo all'utente due parole con due prompt
+
+// Verificho quale delle due è più corta 
+// Stampo prima la parola più corta poi quella più lunga
