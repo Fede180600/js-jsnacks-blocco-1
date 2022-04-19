a8210ad653ff836c5256fd3ad91937f15ef0332f
Fede180600 <fede180600@libero.it>
2022-04-19 16:16:11 +0200

    add usernumber prompt

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,5 +1,6 @@
 // jsnack4. Chiedi un numero di 4 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
 
 // Chiedo all'utente un numero con 4 cifre 
-
+const userNumber = parseInt(prompt("Inserisci un numero con 4 cifre"));
+console.log(userNumber);
 // Calcolo la somma delle cifre che compongono il numero inserito dall'utente
