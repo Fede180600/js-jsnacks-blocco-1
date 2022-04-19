fdfd4ddcf501f1e9c7c28b7c591f7a79f7f64cc2
Fede180600 <fede180600@libero.it>
2022-04-19 15:27:54 +0200

    jsnack2 js psudo-code

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,1 +1,7 @@
 // jsnack2. Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+// Creo la mia array vuota
+
+// Chiedo 6 numeri all'utente 
+// Se è dispari lo inserisco nell'array
+
