55d2f3132aac3e9b2b9c2fb0cb116ba8a3fdd61f
Fede180600 <fede180600@libero.it>
2022-04-19 15:57:45 +0200

    add numbers sum calc

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,10 +1,15 @@
 // jsnack3. Calcola la somma e la media dei primi 10 numeri.
 
 // Generare i primi 10 numeri 
+// Calcolarne la somma 
+let somma = 0;
+console.log("Somma", somma);
+
 for (let i = 1; i <= 10; i++) {
     console.log(i);
+    somma = somma + i;
 }
-// Calcolarne la somma 
 
+console.log("Somma finale", somma);
 
 // Calcolarne la media
