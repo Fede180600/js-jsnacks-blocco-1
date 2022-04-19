42f5ce83622ec953190d239d676d2a37ec262c43
Fede180600 <fede180600@libero.it>
2022-04-19 16:12:01 +0200

    jsnack4 js pseudo-code

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,1 +1,5 @@
 // jsnack4. Chiedi un numero di 4 cifre all'utente e calcola la somma di tutte le cifre che compongono il numero.
+
+// Chiedo all'utente un numero con 4 cifre 
+
+// Calcolo la somma delle cifre che compongono il numero inserito dall'utente
