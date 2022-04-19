9f7d478ccda322b9f13ab71eb379f2045b97531d
Fede180600 <fede180600@libero.it>
2022-04-19 15:11:45 +0200

    add jsnack1 prompts

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,6 +1,9 @@
 // jsnack1. L'utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
 // Chiedo all'utente due parole con due prompt
+const userFirstWord = prompt("Dimmi la prima parola");
+const userSecondWord = prompt("Dimmi la seconda parola");
 
-// Verificho quale delle due è più corta 
+// Verificho la lunghezza delle parole
+
 // Stampo prima la parola più corta poi quella più lunga
